c80ef5b2f4335cb5cf428be238be85b0c8a86ad4
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2026-07-05 07:26:26 +0100

    claf

--- scripts/metal_reg_cv.py
+++ scripts/metal_reg_cv.py
@@ -13,8 +13,12 @@ model_name = 'MLP'
 data_dir = Path('./assets/results')
 plot_path = Path("./assets/outputs") / f"metal_reg_importance_{model_name}_logo.pdf"
 target_dataset = 'monash_m3_monthly'
-# y_clip_min, y_clip_max = -1, 1
-y_clip_min, y_clip_max = 0, 2
+PERFORMANCE_DIFF = False
+
+if PERFORMANCE_DIFF:
+    y_clip_min, y_clip_max = -2.5, 2.5
+else:
+    y_clip_min, y_clip_max = 0, 5
 
 metadata = read_all_metadata(data_dir, model_name, detailed=False)
 
@@ -22,8 +26,11 @@ df_after_train = metadata.query('step==-1').reset_index(drop=True)
 print(df_after_train['dataset'].value_counts())
 
 mase_sn_by_dataset = df_after_train.groupby('dataset')['mase_sn'].first()
-y_reg = df_after_train['mase']
-# y_reg = df_after_train['mase_sn'] - df_after_train['mase']
+if PERFORMANCE_DIFF:
+    y_reg = df_after_train['mase_sn'] - df_after_train['mase']
+else:
+    y_reg = df_after_train['mase']
+
 groups = df_after_train['dataset']
 X = df_after_train.drop(columns=['mase', 'mase_sn', 'model', 'config_id', 'step', 'dataset'])
 
@@ -43,7 +50,7 @@ for train_idx, test_idx in logo.split(X, y_reg, groups):
     reg.fit(X.iloc[train_idx], y_tr)
 
     preds = reg.predict(X.iloc[test_idx])
-    y_baseline = np.repeat(np.median(y_tr), len(y_ts))
+    y_baseline = np.repeat(np.mean(y_tr), len(y_ts))
 
     y_true_folds.append(y_ts)
     pred_folds.append(preds)
@@ -51,8 +58,7 @@ for train_idx, test_idx in logo.split(X, y_reg, groups):
 
     nmae_fold = mae(y_ts, preds) / mae(y_ts, y_baseline)
 
-    thr = mase_sn_by_dataset[held_out]
-    # thr = 0
+    thr = 0 if PERFORMANCE_DIFF else mase_sn_by_dataset[held_out]
     y_exc_bin = (y_ts > thr).astype(int)
     pred_exc = reg.prob_exceeds(X.iloc[test_idx], thr)
 
